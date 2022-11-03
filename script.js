83dcd360bb413c36f231b81e6b72bd3c36f64296
Scott A. Green <sagreenxyz@gmail.com>
2022-11-02 19:16:36 -0500

    adding capability to use api data with fallback to local data on fetching error

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// let apiQuotes = []
+let apiQuotes = []
+
+function getRandomElement(arry) {
+    return arry[Math.floor(Math.random() * arry.length)]
+}
 
 function newQuote() {
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+    randApiQuote = getRandomElement(apiQuotes)
+    randLocalQuote = getRandomElement(localQuotes)
+    const quote = randApiQuote || randLocalQuote
     console.log(quote)
 }
 
 // Get Quotes from API
-// async function getQuotes() {
-//     const apiUrl = 'https://type.fit/api/quotes'
-//     try {
-//         const response = await fetch(apiUrl)
-//         apiQuotes = await response.json()
-//         newQuote()
-//     } catch (error) {
-//         alert(error)
-//     }
-// }
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes'
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json()
+    } catch (error) {
+        alert('Error Fetching Data from Server: Will Use Local Data Instead')
+    } finally {
+        newQuote()
+    }
+}
 
 // On Load
-// getQuotes()
-newQuote()
+getQuotes()
