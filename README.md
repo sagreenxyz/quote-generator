d931882ebc2603204b39503965ec1d2e02cec95d
Scott A. Green <sagreenxyz@gmail.com>
2022-11-03 11:14:50 -0500

    adding loader component

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # quote-generator
 
 [Live Demo](https://sagreenxyz.github.io/quote-generator/)
+
+## Resources
+- [Hero Patterns](heropatterns.com)
+- [W3Schools CSS Loader Example](https://www.w3schools.com/howto/howto_css_loader.asp)
--- script.js
+++ script.js
@@ -3,50 +3,61 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = []
 
-function getRandomElement(arry) {
-    return arry[Math.floor(Math.random() * arry.length)]
+// Show the loader
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
 }
 
+// Stop to show the loader
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
+
+// Show new quote
 function newQuote() {
-    randApiQuote = getRandomElement(apiQuotes)
-    // randLocalQuote = getRandomElement(localQuotes)
-    // const quote = randApiQuote || randLocalQuote
-    const quote = randApiQuote
-    authorText.textContent = quote.author || 'Unknown'
-    
-    // Special format for long-length quote texts
-    if (quote.text.length > 120) {
+    loading()
+    // Pick a random quote from apiQuotes
+    let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    // Check if author field is blank and replace it with 'Unknown'
+    if(!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author
+    }
+    // Check quote length to determine styling
+    if(quote.text.length > 120) {
         quoteText.classList.add('long-quote')
     } else {
         quoteText.classList.remove('long-quote')
     }
-
     quoteText.textContent = quote.text
+    complete()
 }
 
-// Get Quotes from API
 async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes'
+    loading()
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
     try {
         const response = await fetch(apiUrl)
         apiQuotes = await response.json()
-    } catch (error) {
-        alert('Error Fetching Data from Server: Will Use Local Data Instead')
-    } finally {
         newQuote()
+    } catch(error) {
+        // handling error
     }
 }
 
-// Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
-    window.open(twitterUrl, '_blank')
+    const tweet = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(tweet, '_blank')
 }
 
-// On Load
-getQuotes()
-newQuoteBtn.addEventListener('click', getQuotes)
 twitterBtn.addEventListener('click', tweetQuote)
+newQuoteBtn.addEventListener('click', getQuotes)
+
+getQuotes()
