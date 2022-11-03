8469e28983ff8db6568ba34aa794c83b8bc9da4c
Scott A. Green <sagreenxyz@gmail.com>
2022-11-02 19:09:43 -0500

    setting up with local quotes only - removing fetch

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let apiQuotes = []
+// let apiQuotes = []
 
 function newQuote() {
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
     console.log(quote)
 }
 
 // Get Quotes from API
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes'
-    try {
-        const response = await fetch(apiUrl)
-        apiQuotes = await response.json()
-        newQuote()
-    } catch (error) {
-        alert(error)
-    }
-}
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes'
+//     try {
+//         const response = await fetch(apiUrl)
+//         apiQuotes = await response.json()
+//         newQuote()
+//     } catch (error) {
+//         alert(error)
+//     }
+// }
 
 // On Load
-getQuotes()
+// getQuotes()
+newQuote()
